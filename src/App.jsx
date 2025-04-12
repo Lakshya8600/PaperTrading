cdb2d6ff06d5e613815568ba2c7a33398b6b84da
Lakshya8600 <lakshya.kumrawat@gmail.com>
2025-04-12 15:44:39 +0530

    added routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,36 @@
-import { useState } from 'react'
-import './App.css'
-import Navbar from './components/navbar/navbar'
-
+import { useState } from "react";
+import "./App.css";
+import Navbar from "./components/navbar/navbar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <Navbar />
-      
+      <Router>
+        <Routes>
+          <Route path="/" element={<Navbar />} />
+          <Route path="/trade" element={<Navbar />} />
+          <Route path="/login" element={<Navbar />} />
+          <Route path="/report" element={<Contact />} />
+          <Route path="/dashboard" element={<Contact />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Router>
+
+      {/* <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Layout />}>
+          <Route path="/trade" element={<Layout />}>
+          <Route path="/login" element={<Layout />}>
+          <Route path="/report" element={<Layout />}>
+          <Route path="/dashboard" element={<Layout />}>
+        </Routes>
+      </BrowserRouter> */}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,13 +1,9 @@
 import React from 'react'
-import Logo from './logo'
-import Loginbtn from './loginbtn'
-import Searchbox from './searchbox'
-
 
 const Navbar = () => {
   return (
     <div>
-        
+
     </div>
   )
 }
