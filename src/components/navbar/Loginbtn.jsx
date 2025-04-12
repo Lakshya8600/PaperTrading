befc1d324e1408f54aff3605e956f1bafa8cdf7b
Sairaj182 <sairajraithatha.official@gmail.com>
2025-04-12 15:21:01 +0530

    initial setup

---
+++ src/components/navbar/Loginbtn.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Loginbtn = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Loginbtn
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Logo from './logo'
+import Loginbtn from './loginbtn'
+import Searchbox from './searchbox'
+
+
+const Navbar = () => {
+  return (
+    <div>
+      <Logo />
+      <Loginbtn />
+      <Searchbox />
+      
+    </div>
+  )
+}
+
+export default Navbar
