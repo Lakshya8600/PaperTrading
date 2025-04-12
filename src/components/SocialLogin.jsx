753bd2a9901a073b3d53991501543f6fbfab0e05
Sairaj182 <sairajraithatha.official@gmail.com>
2025-04-12 18:11:54 +0530

    login-page-component

---
+++ src/components/SocialLogin.jsx
@@ -0,0 +1,11 @@
+const SocialLogin = () => {
+    return (
+      <div className="social-login">
+        <button className="social-button">
+          <img src="https://logos-world.net/wp-content/uploads/2020/09/Google-Symbol.png" alt="Google" className="social-icon " />
+        </button>
+        
+      </div>
+    )
+  }
+  export default SocialLogin;
